656cbd3602e2edbd05900fc6164821053302c136
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-11-04 13:10:15 -0500

    initial commit

---
+++ src/main/java/com/pluralsight/CreditCard.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+
+public class CreditCard extends Account {
+
+
+    public CreditCard(String name, String accountNumber, double balance) {
+        super(name, accountNumber, balance);
+    }
+
+    public void charge(double amount) {
+        setBalance(getBalance() - amount);
+    }
+
+    public void pay(double amount) {
+        setBalance(getBalance() + amount);
+    }
+
+    @Override
+    public double getValue() {
+        return getBalance();
+    }
+}
