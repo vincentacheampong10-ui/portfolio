660634c3e98550c9bc0804a136d69787151d483b
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-11-04 11:06:17 -0500

    initial commit/fixedAssest

---
+++ src/main/java/com/pluralsight/FixedAsset.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+public abstract class FixedAsset {
+    private String name;
+    private double marketValue;
+
+    public FixedAsset(String name, double marketValue) {
+        this.name = name;
+        this.marketValue = marketValue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public double getMarketValue() {
+        return marketValue;
+    }
+
+    public abstract double getValue();
+}
