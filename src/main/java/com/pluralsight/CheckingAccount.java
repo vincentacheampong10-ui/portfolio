661083f352a6a5cbb8419d30a198b8fb58560015
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-11-04 13:10:42 -0500

    initial commit

---
+++ src/main/java/com/pluralsight/CheckingAccount.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+public class CheckingAccount extends Account {
+
+    public CheckingAccount(String name, String accountNumber, double balance) {
+        super(name, accountNumber, balance);
+    }
+
+    @Override
+    public double getValue() {
+        return getBalance();
+    }
+
+    public double deposit(double amount) {
+        double newBalance = getBalance() + amount;
+        setBalance(newBalance);
+        return newBalance;
+    }
+
+    public double withdraw(double amount) {
+        double newBalance = getBalance() - amount;
+        setBalance(newBalance);
+        return newBalance;
+    }
+}
