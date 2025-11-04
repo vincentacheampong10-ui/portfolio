634b290d650ef0a76fa22762bc47ba8444bcdab5
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-11-04 10:39:01 -0500

    initial commit

---
+++ src/main/java/com/pluralsight/House.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class House {
+    int yearBuilt;
+    int squareFeet;
+    int bedrooms;
+
+    public House(int yearBuilt, int squareFeet, int bedrooms) {
+        this.yearBuilt = yearBuilt;
+        this.squareFeet = squareFeet;
+        this.bedrooms = bedrooms;
+    }
+
+    public void getvalue() {
+    }
+}
